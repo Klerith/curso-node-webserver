c08a0c889367f32a1459c5af12324e5de866585f
Fernando Herrera <fernando.herrera85@gmail.com>
2021-02-03 17:33:37 -0500

    Despliegue app - 2021-02-15

--- app.js
+++ app.js
@@ -12,29 +12,29 @@ hbs.registerPartials( __dirname + '/views/partials');
 // Servir contenido estático
 app.use( express.static('public') );
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-});
+// app.get('/', (req, res) => {
+//     res.render('home', {
+//         nombre: 'Fernando Herrera',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-});
+// app.get('/generic', (req, res) => {
+//     res.render('generic', {
+//         nombre: 'Fernando Herrera',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-});
+// app.get('/elements', (req, res) => {
+//     res.render('elements', {
+//         nombre: 'Fernando Herrera',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
 app.get('*', (req, res) => {
-    res.sendFile( __dirname + '/public/404.html');
+    res.sendFile( __dirname + '/public/index.html');
 });
  
 app.listen(port, () => {
